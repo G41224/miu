c964ea9a69ff21ed01cb583f6a63ddfb81dfef79
Zachery croasmun <th3p4ler1der@gmail.com>
2013-05-06 20:51:08 -0700

    all files from last month added

---
+++ js/json.js
@@ -0,0 +1,16 @@
+/**
+ * @author Zac Croasmun
+ */
+
+var json = {
+	
+	"drinks": {
+		"name": ["Drink Name:", "jack and coke"],
+		"type": ["Drink Type:", "Alcoholic" ],
+		"date": ["date:", "10/10/2010"],
+		"note": ["note:", "this is a good drink"]
+	}
+	
+	
+	
+};
